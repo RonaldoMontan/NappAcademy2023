383c2c22bb8cea1f01f360e7519835d82a499c43
Ronaldo Montan <ronaldogithub@gmail.com>
2023-02-25 09:33:37 -0300

    recomeçando o projeto, estavamos seguindo um caminho errado

--- README.md
+++
@@ -1,1 +0,0 @@
-# NappAcademy2023
--- back.py
+++ back.py
@@ -1,92 +1,24 @@
-from flask import Flask, request
-from flask_cors import CORS
-from back_mongo import Mongo
-from datetime import datetime
-import json
+from flask import Flask
+import sqlite3 as lite
 
-
-classe = Mongo()
 
 app = Flask(__name__)
-cors = CORS(app)
 
-
-#===QUADRA===
-
-@app.route("/inf_quadra", methods=['POST'])
-def inf_quadra():
-
-    data = json.loads(request.data)
-    chamada = classe.find_one_quadra(data)
-    return chamada
-
-@app.route("/quadra", methods=['POST'])
-def quadra():
-    data = [json.loads(request.data)]
-    chamada = classe.insert_many_quadra(data)
-    return chamada
-
-@app.route("/del_quadra", methods=['POST'])
-def del_quadra():
-
-    data = json.loads(request.data)
-    chamada = classe.delete_mongo_quadra(data)
-    return chamada
-
-@app.route("/up_quadra", methods=['POST'])
-def up_quadra():
-    data = json.loads(request.data)
-    chamada = classe.delete_mongo_quadra(data)
-    return chamada
-
-
-#===CLIENTE===
-
-
-@app.route("/cliente", methods=['POST'])
+@app.route("/cliente", methods=["POST"])
 def cliente():
+    return "inserir dados cliente"
 
-    data = [json.loads(request.data)]
-    chamada = classe.inser_many_cliente(data)    
-    return chamada
-
-
-@app.route("/inf_cliente",  methods=['POST'])
-def inf_cliente():
-
-    data = json.loads(request.data)
-    chamada = classe.find_one_cliente(data)
-    return (chamada)
+con = lite.connect('quadra.db')
 
 
-@app.route("/del_cliente", methods=["POST"])
-def del_cliente():
-
-    data = json.loads(request.data)
-    chamada = classe.delete_mongo_cliente(data)
-    return chamada
+cur = con.cursor()
 
 
-@app.route("/up_cliente", methods=["POST"])
-def up_cliente():
-
-    data = json.loads(request.data)
-    chamada = classe.update_mongo_cliente(data)
-    return chamada
-
-
-
-# ====quadra-cliente=====
-@app.route("/solicitacao-quadra", methods=["PUT"])
-def solicitacao_quadra():
-    id_cliente = json.loads(request.data)
-
-    print(f"[ID_CLIENTE] {id_cliente}")
-    document = classe.find_one_cliente(id_cliente)['document']
-    print(f"[DOCUMENTS] {document}")
-
-    classe.update_mongo_quadra_cliente(documents_client=document, documents_quadra=id_cliente)
-
-    return {'porra':'nenhuma'}
-
-app.run(port=3030)
+cur.execute("""
+    select * from Quadra q 
+    inner join Cliente c on q.id_cliente = c.id_cliente 
+    inner join Horarios h on q.id_horario = h.id_horario 
+    inner join Reserva r on q.id_reserva = r.id_reserva 
+    """)
+consulta = cur.fetchall()
+print(consulta)
